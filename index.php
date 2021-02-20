a595fa51e1ac31f756cc0354280b14456b6dca72
Afonso-cmd <afonso.ur@gmail.com>
2021-02-20 15:58:22 -0300

    Busca

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once "vendor/autoload.php";
+
+use \Wead\DigitalCep\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipcode('08121600');
+
+print_r($resultado);
