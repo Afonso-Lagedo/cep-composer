1b3eed74373012eb1456a6b54def7bad2a8f3d4a
Afonso-cmd <afonso.ur@gmail.com>
2021-02-22 19:11:35 -0300

    PHPUnit teste classe Search

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -6,9 +6,26 @@ use Wead\DigitalCep\Search;//classe testada
 
 class SearchTest extends TestCase{// Comanda todos os testes
     public function testGetAddressFromZipcodeDefaultUsage(){//padrão:test+ nome função testada +DefaultUsage, mas pode ser qualquer nome
-        $resultado = "ok";
+        $resultado = new Search;
+        $resultado = $resultado->getAddressFromZipcode('08121600');
 
-        $this->assertEquals("ok", $resultado);
+        $esperado =[
+
+            "cep" => "08121-600", 
+            "logradouro" => "Rua Damião de Sousa Pereira",
+            "complemento" => "",
+            "bairro" => "Jardim São Luís (Zona Leste)",
+            "localidade" => "São Paulo",
+            "uf" => "SP", 
+            "ibge" => "3550308", 
+            "gia" => "1004",
+            "ddd" => "11", 
+            "siafi" => "7107"
+
+        ];
+
+
+        $this->assertEquals($esperado, $resultado);
     }
 }
 
