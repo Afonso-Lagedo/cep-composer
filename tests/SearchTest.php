4edb1ba6b93880d1f9e1c3d2bb5f8858283b1223
Afonso-cmd <afonso.ur@gmail.com>
2021-02-22 20:00:08 -0300

    PHPUnit Coleção de testes

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -5,27 +5,52 @@ use PHPUnit\Framework\TestCase;
 use Wead\DigitalCep\Search;//classe testada
 
 class SearchTest extends TestCase{// Comanda todos os testes
-    public function testGetAddressFromZipcodeDefaultUsage(){//padrão:test+ nome função testada +DefaultUsage, mas pode ser qualquer nome
+    /**
+     * @dataProvider dadosTeste 
+     **/
+    
+    public function testGetAddressFromZipcodeDefaultUsage(string $input, array $esperado){//padrão:test+ nome função testada +DefaultUsage, mas pode ser qualquer nome
         $resultado = new Search;
-        $resultado = $resultado->getAddressFromZipcode('08121600');
-
-        $esperado =[
-
-            "cep" => "08121-600", 
-            "logradouro" => "Rua Damião de Sousa Pereira",
-            "complemento" => "",
-            "bairro" => "Jardim São Luís (Zona Leste)",
-            "localidade" => "São Paulo",
-            "uf" => "SP", 
-            "ibge" => "3550308", 
-            "gia" => "1004",
-            "ddd" => "11", 
-            "siafi" => "7107"
-
-        ];
-
+        $resultado = $resultado->getAddressFromZipcode($input);
 
         $this->assertEquals($esperado, $resultado);
+    }
+
+    //coleção de testes
+
+    public function dadosTeste(){
+        return[
+            "Primeiro Endereço"=>[
+                "08121-600",
+                [
+                    "cep" => "08121-600", 
+                    "logradouro" => "Rua Damião de Sousa Pereira",
+                    "complemento" => "",
+                    "bairro" => "Jardim São Luís (Zona Leste)",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP", 
+                    "ibge" => "3550308", 
+                    "gia" => "1004",
+                    "ddd" => "11", 
+                    "siafi" => "7107"
+                ]
+            ],
+            "Segundo Endereço"=>[
+                "08121-600",
+                [
+                    "cep" => "08121-600", 
+                    "logradouro" => "Rua Damião de Sousa Pereira",
+                    "complemento" => "",
+                    "bairro" => "Jardim São Luís (Zona Leste)",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP", 
+                    "ibge" => "3550308", 
+                    "gia" => "1004",
+                    "ddd" => "11", 
+                    "siafi" => "7107"
+                ]
+            ]
+        ];
     }
 }
 
